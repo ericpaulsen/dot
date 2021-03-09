0f9710763dcc63dc7d0475668dff3b0167c22a59
Eric Paulsen <ericpaulsen@coder.com>
2021-03-09 15:49:32 -0600

    add: install script

---
+++ install.sh
@@ -0,0 +1,14 @@
+# copy dotfiles into ~
+shopt -s dotglob # include . in *
+shopt -s extglob
+yes | cp -rf ~/dotfiles/!(.git|.|..|.local) ~
+# Set VS Code preferences for the FIRST time
+if [ -f "/home/coder/.local/share/code-server/User/settings.json" ] 
+then
+    echo "VS Code settings are already present." 
+else
+    cp -rf /home/coder/dotfiles/.local ~/.local
+    # Install extensions
+    /opt/coder/code-server/bin/code-server --install-extension esbenp.prettier-vscode
+    /opt/coder/code-server/bin/code-server --install-extension daylerees.rainglow
+fi
