9a0783de5fbe25ed10a111ca64b88e72018a9e4d
Eric Paulsen <ericpaulsen@coder.com>
2021-04-01 22:56:55 -0500

    add fish install

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@ shopt -s dotglob # include . in *
 shopt -s extglob
 yes | cp -rf ~/dotfiles/!(.git|.|..|.local) ~
 # Set VS Code preferences for the FIRST time
+
+echo "installing extensions..."
+
 if [ -f "settings.json" ] 
 then
     cp -rf /home/coder/dotfiles/.local ~/.local
@@ -10,3 +13,19 @@ then
     /opt/coder/code-server/bin/code-server --install-extension yummygum.city-lights-theme
     /opt/coder/code-server/bin/code-server --install-extension streetsidesoftware.code-spell-checker
 fi
+
+echo "install fish shell"
+
+FISH_BINARY=/usr/bin/fish
+FISH_PATH=/usr/bin
+
+if [ ! -f $FISH_BINARY ] ; then
+    sudo apt-get update
+    sudo apt-get install -y fish
+    echo "installing fish in $FISH_PATH"
+else
+    echo "fish already installed"
+fi
+
+echo "changing shell"
+sudo chsh -s /usr/bin/fish $USER
