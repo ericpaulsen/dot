bc8a9074fcc804f66139199bb6122b85a30b2b6b
Eric Paulsen <ericpaulsen@coder.com>
2021-04-01 22:28:22 -0500

    update install.sh & settings.json

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@ shopt -s dotglob # include . in *
 shopt -s extglob
 yes | cp -rf ~/dotfiles/!(.git|.|..|.local) ~
 # Set VS Code preferences for the FIRST time
-if [ -f "/home/coder/.local/share/code-server/User/settings.json" ] 
+if [ -f "settings.json" ] 
 then
     echo "VS Code settings are already present." 
 else
     cp -rf /home/coder/dotfiles/.local ~/.local
     # Install extensions
-    /opt/coder/code-server/bin/code-server --install-extension zhuangtongfa.material-theme
+    /opt/coder/code-server/bin/code-server --install-extension yummygum.city-lights-theme
 fi
