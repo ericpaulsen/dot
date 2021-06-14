8ce4d5ec510429f60dc980ad596b1decfa2cdfb2
Eric Paulsen <ericpaulsen@coder.com>
2021-06-14 14:49:38 -0500

    fix: if syntax

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ echo "install fish shell"
 
 DISTRO=$(egrep '^(NAME)=' /etc/os-release)
 
-if [$DISTRO = "NAME=Arch Linux"]; then
+if [ $DISTRO =~ "NAME=Arch Linux" ]; then
     yes | sudo pacman -S fish
     FISH_PATH=$(which fish)
     export PATH=$PATH:FISH_PATH
