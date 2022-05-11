9c5b2b92238a2393671ba76a97ebe7708ce115bb
Eric Paulsen <ericpaulsen@coder.com>
2022-05-11 12:27:37 -0500

    add theme vsix file & fix starship install

--- install.sh
+++ install.sh
@@ -3,16 +3,28 @@ shopt -s dotglob # include . in *
 shopt -s extglob
 yes | cp -rf ~/dotfiles/!(.git|.|..|.local) ~
 
+# Install kubectl
+brew install kubectl
+
+# Install kubectx
+sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
+sudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
+sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
+
+# Install terraform
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update && sudo apt-get install terraform
+
 # Set VS Code preferences
 echo "installing extensions..."
 
 if [[ -f "settings.json" ]] 
 then
-    cp -rf /home/coder/dotfiles/.local ~/.local
     # Install extensions
     /var/tmp/coder/code-server/bin/code-server --install-extension pkief.material-icon-theme
     /var/tmp/coder/code-server/bin/code-server --install-extension streetsidesoftware.code-spell-checker
-    /var/tmp/coder/code-server/bin/code-server --install-extension marnix.tokyo-night-pro
+    /var/tmp/coder/code-server/bin/code-server --install-extension /vsix/marnix.tokyo-night-pro-1.1.4.vsix
 fi
 
 # Install fish & make it default shell
@@ -42,35 +54,14 @@ else
     fi
     echo "changing shell"
     sudo chsh -s /usr/bin/fish $USER
-    
-    # Install OMF
-    git clone https://github.com/oh-my-fish/oh-my-fish
-    cd oh-my-fish/
-    bin/install --offline
-    omf install neolambda
-
-    # Install Starship
-    curl -sS https://starship.rs/install.sh | sh
 
 fi
 
-# Install kubectl
-curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+# Install OMF
+echo "installing OMF"
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+omf install neolambda
 
-# Install kubectx
-sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
-sudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
-sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
-
-# Install terraform
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-sudo apt-get update && sudo apt-get install terraform
-
-# Install gcloud
-curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-385.0.0-linux-x86_64.tar.gz
-tar -xf google-cloud-cli-385.0.0-linux-x86.tar.gz
-dotfiles/./google-cloud-sdk/install.sh
-dotfiles./google-cloud-sdk/bin/gcloud init
-
+echo "installing starship"
+# Install Starship
+brew install starship
